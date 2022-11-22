176651679be74f26c2be8ce19c3404287312cb2e
poro625 <poro625@naver.com>
2022-11-22 20:27:34 +0900

    [수정] 좋아요

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from articles import views
 
+
 urlpatterns = [
 
-    path('<int:movie_id>/like/', views.ArticlesFeedLikeView.as_view(), name = 'movie_like_view'), #좋아요
+    path('<int:feed_id>/like/', views.ArticlesFeedLikeView.as_view(), name = 'feed_like_view'), #좋아요
     path('search/', views.ArticlesSearchView.as_view(), name = 'articles_search_view'), #검색
 ]
 
--- articles/views.py
+++ articles/views.py
@@ -9,15 +9,16 @@ from rest_framework import filters
 from rest_framework import permissions
 from articles.serializers import ArticleSerializer
 
-class ArticlesFeedLikeView(APIView): # 좋아요
+
+class ArticlesFeedLikeView(APIView): # Feed 좋아요
     permission_classes = [permissions.IsAuthenticated]
-    def post(self, request,movie_id ):
-        article = get_object_or_404(Feed, id=movie_id)
-        if request.user in article.movie_like.all():
-            article.movie_like.remove(request.user)
+    def post(self, request,feed_id ):
+        feed = get_object_or_404(Feed, id=feed_id)
+        if request.user in feed.like.all():
+            feed.like.remove(request.user)
             return Response("좋아요취소했습니다", status=status.HTTP_200_OK)
         else:
-            article.movie_like.add(request.user)
+            feed.like.add(request.user)
             return Response("좋아요했습니다", status=status.HTTP_200_OK)
 
 class ArticlesSearchView(generics.ListAPIView): #검색
@@ -25,4 +26,4 @@ class ArticlesSearchView(generics.ListAPIView): #검색
 
     filter_backends = [filters.SearchFilter]
     # 검색 키워드를 지정했을 때, 매칭을 시도할 필드
-    search_fields = ["category__name","title","description"]
+    search_fields = ["title","description"]
