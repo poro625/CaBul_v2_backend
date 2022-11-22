2f5ef80534d7ac74f2310d5ecc34dbb1b29d7943
poro625 <poro625@naver.com>
2022-11-22 16:44:31 +0900

    [추가] 검색

--- articles/views.py
+++ articles/views.py
@@ -3,10 +3,14 @@ from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from rest_framework import status
 from articles import serializers
-from articles.models import Feed, Comment
+from articles.models import Feed
+from rest_framework import generics
+from rest_framework import filters
+from rest_framework import permissions
 
 
 class ArticlesMovieLikeView(APIView): # 좋아요
+    permission_classes = [permissions.IsAuthenticated]
     def post(self, request,movie_id ):
         article = get_object_or_404(Feed, id=movie_id)
         if request.user in article.movie_like.all():
@@ -15,3 +19,10 @@ class ArticlesMovieLikeView(APIView): # 좋아요
         else:
             article.movie_like.add(request.user)
             return Response("좋아요했습니다", status=status.HTTP_200_OK)
+
+class ArticlesSearchView(generics.ListAPIView): #검색
+    serializer_class = ArticleSerializer
+
+    filter_backends = [filters.SearchFilter]
+    # 검색 키워드를 지정했을 때, 매칭을 시도할 필드
+    search_fields = ["category__name","title","description"]
