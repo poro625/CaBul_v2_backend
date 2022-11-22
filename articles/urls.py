388b4d31c19d579171e37c94bcafadf27e3ff0bf
hanmariyang <hanmari.code@gmail.com>
2022-11-22 16:46:07 +0900

    [수정] articles/urls.py (url 수정)

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-
-    path('', views.ArticlesView, name ='articles_view' ),
+    path('', views.ArticlesFeedView.as_view(), name ='feed_upload_view' ), # 게시글 업로드
+    path('/<int:feed_id>/', views.ArticlesFeedDetailView.as_view(), name ='feed_detail_view' ), # 게시글 수정, 삭제
 ]
 
