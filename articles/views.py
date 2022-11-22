f4e714db6ab52e8d3dfdb563aa970a69b036401d
poro625 <poro625@naver.com>
2022-11-22 16:32:22 +0900

    [추가] 좋아요

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from rest_framework import status
+from articles import serializers
+from articles.models import Feed, Comment
 
-# Create your views here.  
 
-def ArticlesView():
-    pass
+class ArticlesMovieLikeView(APIView): # 좋아요
+    def post(self, request,movie_id ):
+        article = get_object_or_404(Feed, id=movie_id)
+        if request.user in article.movie_like.all():
+            article.movie_like.remove(request.user)
+            return Response("좋아요취소했습니다", status=status.HTTP_200_OK)
+        else:
+            article.movie_like.add(request.user)
+            return Response("좋아요했습니다", status=status.HTTP_200_OK)
