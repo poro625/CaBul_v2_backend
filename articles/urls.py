eb5108f6d850255ae88dcdbaa86b51623cf8cc1f
hanmariyang <hanmari.code@gmail.com>
2022-11-22 17:41:12 +0900

    [삭제] 오타 부분 삭제

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,6 @@ from articles import views
 
 urlpatterns = [
     path('', views.ArticlesFeedView.as_view(), name ='feed_upload_view' ), # 게시글 업로드
-    path('/<int:feed_id>/', views.ArticlesFeedDetailView.as_view(), name ='feed_detail_view' ), # 게시글 수정, 삭제
+    path('<int:feed_id>/', views.ArticlesFeedDetailView.as_view(), name ='feed_detail_view' ), # 게시글 수정, 삭제
 ]
 
