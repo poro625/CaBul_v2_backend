be532cdace03dc98f0421f504673ffea64e29647
hanmariyang <hanmari.code@gmail.com>
2022-11-22 16:48:28 +0900

    [기타] user/models.py 임시 추가

--- users/models.py
+++ users/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
+from django.conf import settings
 
 # Create your models here.
-class User(models.Model):
-    pass
+class User(AbstractUser): 
+    email = models.EmailField(verbose_name = "email", max_length = 255, unique = True)
+    username = models.CharField(max_length=30)
+    
+    USERNAME_FIELD = 'email' 
+    REQUIRED_FIELDS = ['username']
+    
