ba3e478b528033a07cff28dff288a6aae9b5a9fa
devjunseok <wnslgood58@naver.com>
2022-11-24 08:53:49 +0900

    [추가] 회원가입 Test 코드 추가

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from users.models import User
 
-# Create your tests here.
+class UserRegistrationTest(APITestCase):
+    def test_registration(self):  #회원가입 테스트
+        url = reverse("rest_register")   # url name
+        user_data = {
+                "email":"test@naver.com",
+                "name":"name",
+                "nickname":"junseok",
+                "password1":"password123@",
+                "password2":"password123@"
+            }
+        response = self.client.post(url, user_data)  # APITestCase의 기본적인 세팅
+        self.assertEqual(response.status_code, 201)
