b5f3353325dee594a3ae57f118f5f8b0ed9a95e9
poro625 <poro625@naver.com>
2022-11-23 10:35:11 +0900

    [추가] 테그 기능

--- articles/urls.py
+++ articles/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<int:feed_id>/', views.ArticlesFeedDetailView.as_view(), name ='feed_detail_view' ),     # 게시글 상세조회, 수정, 삭제
     path('<int:feed_id>/comment/', views.FeedCommentView.as_view(), name = 'feed_comment_view'),   # 댓글 작성
     path('<int:feed_id>/comment/<int:comment_id>/', views.FeedCommentDetailView.as_view(), name = 'feed_comment_detail_view'), # 댓글 수정, 삭제
-    
+    path('tag/', views.TagCloudTV.as_view(), name='tag_cloud'),                                    #테그  #TemplateView를 상속받아 정의
+    path('tag/<str:tag>/', views.TaggedObjectLV.as_view(), name='tagged_object_list'),             #테그  #ListView를 상속받아 정의
 ]
 
