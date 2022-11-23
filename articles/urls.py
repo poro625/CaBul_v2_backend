1768ee4e8fb4bdf3703f093f4eb8bd0ad1a8fc02
poro625 <poro625@naver.com>
2022-11-23 10:33:20 +0900

    Update urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('<int:feed_id>/', views.ArticlesFeedDetailView.as_view(), name ='feed_detail_view' ),     # 게시글 상세조회, 수정, 삭제
     path('<int:feed_id>/comment/', views.FeedCommentView.as_view(), name = 'feed_comment_view'),   # 댓글 작성
     path('<int:feed_id>/comment/<int:comment_id>/', views.FeedCommentDetailView.as_view(), name = 'feed_comment_detail_view'), # 댓글 수정, 삭제
-
+    
 ]
 
